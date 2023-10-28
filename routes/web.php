d61fff51ed4bb313e0289913c0324ffb5f095a0f
Syahnan <syhnnzhra@gmail.com>
2023-10-28 14:16:50 +0700

    Tugas video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about',[
+        "name"=> "Syahnan Azahra",
+        "email"=> "syahnan.213040108@mail.unpas.ac.id",
+        "image"=> "nan.jpg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });

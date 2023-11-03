061734356a872b44b092d8083056287ca7a246aa
Syahnan <syhnnzhra@gmail.com>
2023-11-03 14:24:23 +0700

    tugas video 5,6

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
     <h1>About</h1>
     <h2> {{ $name }}</h2>
     <h3> {{ $email }}</h3>
-    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,52 +29,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_post = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Syahnan Azahra",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem et dolore sint, dolorem unde, amet pariatur adipisci excepturi facilis, inventore cumque error. Aliquid, nisi doloremque corrupti esse laboriosam ea hic nostrum dolore iusto aperiam, officia, voluptate laudantium beatae placeat autem fugit alias asperiores voluptates id voluptatum. Corporis omnis consectetur iusto temporibus sed corrupti minima a iste, neque sint molestias distinctio deleniti aperiam ut nulla aliquam facilis totam exercitationem doloremque delectus optio? Ad fugiat quis quia quasi, voluptates laborum enim."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "AlBarra Permana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem et dolore sint, dolorem unde, amet pariatur adipisci excepturi facilis, inventore cumque error. Aliquid, nisi doloremque corrupti esse laboriosam ea hic nostrum dolore iusto aperiam, officia, voluptate laudantium beatae placeat autem fugit alias asperiores voluptates id voluptatum. Corporis omnis consectetur iusto temporibus sed corrupti minima a iste, neque sint molestias distinctio deleniti aperiam ut nulla aliquam facilis totam exercitationem doloremque delectus optio? Ad fugiat quis quia quasi, voluptates laborum enim."
-        ]
-    ];
-    return view('posts', [
-        "title" => "posts",
-        "posts" => $blog_post
-    ]);
-});
-
-Route::get('posts/{slug}', function($slug){
-    $blog_post = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Syahnan Azahra",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem et dolore sint, dolorem unde, amet pariatur adipisci excepturi facilis, inventore cumque error. Aliquid, nisi doloremque corrupti esse laboriosam ea hic nostrum dolore iusto aperiam, officia, voluptate laudantium beatae placeat autem fugit alias asperiores voluptates id voluptatum. Corporis omnis consectetur iusto temporibus sed corrupti minima a iste, neque sint molestias distinctio deleniti aperiam ut nulla aliquam facilis totam exercitationem doloremque delectus optio? Ad fugiat quis quia quasi, voluptates laborum enim."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "AlBarra Permana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem et dolore sint, dolorem unde, amet pariatur adipisci excepturi facilis, inventore cumque error. Aliquid, nisi doloremque corrupti esse laboriosam ea hic nostrum dolore iusto aperiam, officia, voluptate laudantium beatae placeat autem fugit alias asperiores voluptates id voluptatum. Corporis omnis consectetur iusto temporibus sed corrupti minima a iste, neque sint molestias distinctio deleniti aperiam ut nulla aliquam facilis totam exercitationem doloremque delectus optio? Ad fugiat quis quia quasi, voluptates laborum enim."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_post as $post){
-        if($post["slug"] == $slug){
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
